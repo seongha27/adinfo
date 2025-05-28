d659c609e6cb90bf1726d5b2d2cc489266699a7a
성하의 맥 <seongha@seonghauiui-MacBookAir.local>
2025-05-29 01:21:59 +0900

    최신

--- models/db.js
+++ models/db.js
@@ -2,9 +2,14 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "127.0.0.1", // ← 이 부분!
+  port: process.env.DB_PORT || 3307,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
 module.exports = pool;
